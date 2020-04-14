97289afe494fc8df4a13b6925826f93f83b09e73
Flavian Imlig <flavian@imlig.info>
2020-04-14 13:24:12 +0200

    prepareLV.R - new functions readMeta(), getData(), testTable() data/lv.csv, data/lv_meta.json, Bildung_Lehrvertraege.csv - new indicator

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# covid19_edu_bista
-Daten Bildungsstatistik im Gesellschaftsmonitoring COVID19 
+---
+pagetitle: Gesellschaftsmonitoring COVID19, Daten BISTA
+---
+
+![](https://github.com/bildungsmonitoringZH/bildungsmonitoringZH.github.io/raw/master/assets/ktzh_bi_logo_de-300x88.jpg)
+
+# Gesellschaftsmonitoring COVID19, Daten Bildungsstatistik
+
+Daten der Bildungsstatistik im Rahmen des Projekts [Gesellschaftsmonitoring COVID19](https://statistikzh.github.io/covid19monitoring/)
+
+## Datenlieferant
+
+[source] Kanton Zürich, Bildungsstatistik
+
+## Datenbestände
+
+* [Anzahl abgeschlossener Lehrverträge](https://pub.bista.zh.ch/de/zahlen-und-fakten/andere/lehrvertraege/)
---
+++ prepareLV.R
@@ -0,0 +1,53 @@
+# prepare BISTA LV data for monitoring covid19
+#
+# Authors: Flavian Imlig <flavian.imlig@bi.zh.ch>
+# Date: 14.04.2020
+###############################################################################
+
+readMeta <- function(file = 'data/lv_meta.json')
+{
+    meta <- jsonlite::read_json(file)
+}
+
+getData <- function(file = 'data/lv.csv')
+{
+    data_raw <- read.csv(file, check.names = TRUE)
+    assert_that(ncol(data_raw) == 13)
+    
+    data_t <- data_raw %>%
+        rename_all(~c('jahr', 1:12)) %>%
+        gather('monat', 'value', -.data$jahr) %>%
+        mutate_all(as.integer) %>%
+        arrange(.data$jahr, .data$monat) %>% 
+        drop_na() %>%
+        mutate('date' := as.POSIXct(str_c(.data$jahr, .data$monat, '1', sep = '-')))
+    
+    meta <- readMeta() %>%
+        purrr::imap_dfc(~tibble(!!.y := .x[[1]]))
+    
+    df_spec <- readRDS(url('https://github.com/bildungsmonitoringZH/covid19_edu_mindsteps/raw/master/df_spec.rds'))
+    
+    data_out <- data_t %>%
+        bind_cols(meta %>% slice(rep(1, nrow(data_t)))) %>%
+        select(df_spec$name)
+}
+
+testTable <- function(df)
+{
+    df_spec <- readRDS(url('https://github.com/bildungsmonitoringZH/covid19_edu_mindsteps/raw/master/df_spec.rds'))
+    
+    assert_that(is(df, 'data.frame'))
+    assert_that(identical(names(df), df_spec$name))
+    
+    purrr::pwalk(as.list(df_spec), ~assert_that(is(get(.x, df), .y)))
+    
+    v <- df %>% filter(.data$unit %in% 'Anteil') %>% pull(.data$value)
+    assert_that(all(v <= 1))
+    
+    return(invisible(NULL))
+}
+
+# main
+dat_prep <- getData()
+test <- testTable(dat_prep)
+write.table(dat_prep, "./Bildung_Lehrvertraege.csv", sep=",", fileEncoding="UTF-8", row.names = F)
